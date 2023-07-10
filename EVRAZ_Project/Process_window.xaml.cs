8ed07cca06b72cf03f2d16b51a29e287e37dcbdc
negina.daria <negina.daria@gmail.com>
2023-07-10 12:49:49 +0700

    Созданы все необходимый для программы окна

---
+++ EVRAZ_Project/Process_window.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EVRAZ_Project
+{
+    /// <summary>
+    /// Логика взаимодействия для Process_window.xaml
+    /// </summary>
+    public partial class Process_window : Window
+    {
+        public Process_window()
+        {
+            InitializeComponent();
+        }
+    }
+}
